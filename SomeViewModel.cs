134a76411796321a0a4ac67798f9ce081071a965
Elias G <83478605+eliasgoette@users.noreply.github.com>
2024-03-04 11:04:36 +0100

    Implemented RelayCommand

--- SomeViewModel.cs
+++ SomeViewModel.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace DataBinding_Notes_Test
 {
-    public class SomeViewModel
+    public class SomeViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private SomeModel Model = new();
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private Action _action;
+
+        public RelayCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _action?.Invoke();
+        }
     }
 }
