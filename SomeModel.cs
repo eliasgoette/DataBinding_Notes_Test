179288a2019810c243185ff2a6c04ea180bd2923
Elias G <83478605+eliasgoette@users.noreply.github.com>
2024-03-04 10:57:07 +0100

    Implemented model

---
+++ SomeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBinding_Notes_Test
+{
+    public class SomeModel
+    {
+        public string SomeTextProperty {  get; set; }
+
+        public void ClearText()
+        {
+            SomeTextProperty = String.Empty;
+        }
+    }
+}
